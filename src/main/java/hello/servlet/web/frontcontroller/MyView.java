8650931a1005c2500c5ef541a80e8540a4040a7c
SHEOMM <esh0821@snu.ac.kr>
2023-08-27 01:05:32 +0900

    feat : 프론트 컨트롤러 version 3 구현
    version 2와 차이점 : 서블릿 종속성 제거, 뷰 이름 중복 제거

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -1,6 +1,7 @@
 package hello.servlet.web.frontcontroller;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,5 +19,11 @@ public class MyView {
     }
     public String getViewPath() {
         return viewPath;
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+        model.forEach(request::setAttribute);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
     }
 }
