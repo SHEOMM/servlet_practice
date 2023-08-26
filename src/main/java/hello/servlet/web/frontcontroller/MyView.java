992c40dc00d006ba457b40cd9fc8991f8ca25247
SHEOMM <esh0821@snu.ac.kr>
2023-08-27 00:49:17 +0900

    feat : 프론트 컨트롤러 version 2 구현
    version 1과 차이점 : render 파트를 역할 분리

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.frontcontroller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyView {
+    private final String viewPath; // 논리 이름
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+    public String getViewPath() {
+        return viewPath;
+    }
+}
